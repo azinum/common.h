b0555d31324b2867a3e80d07a273e108aa18fa26
azinum <lukaska61@gmail.com>
2024-01-24 13:08:45 +0100

    Do not run apt/brew update in CI script. Update build.sh.

--- build.sh
+++ build.sh
@@ -9,4 +9,5 @@ set -xe
 
 ${CC} test.c -o test ${FLAGS} -O2 -DNPROC=${NPROC} -DCACHELINESIZE=${CACHELINESIZE} -lpthread
 which x86_64-w64-mingw32-gcc 1>/dev/null && CC=x86_64-w64-mingw32-gcc && \
-${CC} test.c -o test ${FLAGS} -O2 -DNPROC=${NPROC} -DCACHELINESIZE=${CACHELINESIZE} -lpthread
+	${CC} test.c -o test ${FLAGS} -O2 -DNPROC=${NPROC} -DCACHELINESIZE=${CACHELINESIZE} \
+	|| exit 0
