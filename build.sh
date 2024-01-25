a80841f9a9775129b301a68fe39e2be830d5bddb
azinum <lukaska61@gmail.com>
2024-01-25 12:08:35 +0100

    Test add windows to CI build with a different gcc setup

--- build.sh
+++ build.sh
@@ -8,6 +8,3 @@ CACHELINESIZE=`getconf LEVEL1_DCACHE_LINESIZE`
 set -xe
 
 ${CC} test.c -o test ${FLAGS} -O2 -DNPROC=${NPROC} -DCACHELINESIZE=${CACHELINESIZE} -lpthread
-which x86_64-w64-mingw32-gcc 1>/dev/null && CC=x86_64-w64-mingw32-gcc && \
-	${CC} test.c -o test ${FLAGS} -O2 -DNPROC=${NPROC} -DCACHELINESIZE=${CACHELINESIZE} \
-	|| exit 0
