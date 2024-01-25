e3fabeff772a5a694b11497f6ea00dd69295298a
azinum <lukaska61@gmail.com>
2024-01-25 12:17:33 +0100

    Use hardlinks when setup MinGW

--- build.bat
+++ build.bat
@@ -7,6 +7,4 @@ set LIBS=
 set INC=.
 set FLAGS=-I%INC% -Wall -O2 -DNPROC=%NUMBER_OF_PROCESSORS%
 
-%CC% test_empty.c -o test_empty.exe %LIBS% %INC% %FLAGS%
-
-:: %CC% test.c -o test.exe %LIBS% %INC% %FLAGS%
+%CC% test.c -o test.exe %LIBS% %INC% %FLAGS%
--- test_empty.c
+++
@@ -1,5 +0,0 @@
-// test_empty.c
-
-int main(void) {
-  return 0;
-}
