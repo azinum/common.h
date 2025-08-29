a51973f10497e83a47f0df470feadf28adc21e17
azinum <lukaska61@gmail.com>
2025-08-29 17:12:33 +0200

    Update build.bat

--- tests/build.bat
+++ tests/build.bat
@@ -11,9 +11,13 @@ set FLAGS=-Wall -O2 -I.. -DNPROC=%NUMBER_OF_PROCESSORS% -DVERBOSE
 %CC% test_thread_sync.c -o test_thread_sync.exe %LIBS% %INC% %FLAGS%
 %CC% test_arena.c -o test_arena.exe %LIBS% %INC% %FLAGS%
 %CC% test_random.c -o test_random.exe %LIBS% %INC% %FLAGS%
+%CC% test_timer.c -o test_timer.exe %LIBS% %INC% %FLAGS%
+%CC% test_log.c -o test_log.exe %LIBS% %INC% %FLAGS%
 
 test_thread.exe
 test_thread_with_mutex.exe
 test_thread_sync.exe
 test_arena.exe
 test_random.exe
+test_timer.exe
+test_log.exe
