7f8c19d4ad2abbc8d2babf1010e77202da5711b9
azinum <lukaska61@gmail.com>
2025-10-07 10:31:25 +0200

    Make random.h independent from common.h

--- random.h
+++ random.h
@@ -3,16 +3,26 @@
 #ifndef _RANDOM_H
 #define _RANDOM_H
 
+#ifdef __cplusplus
+  #define RESTRICT
+  #define RANDOM_PUBLICDEC extern "C"
+  #define RANDOM_PUBLICDEF extern "C"
+#else
+  #define RESTRICT restrict
+  #define RANDOM_PUBLICDEC extern
+  #define RANDOM_PUBLICDEF
+#endif
+
 #ifndef Random
   typedef size_t Random;
 #endif
 
-COMMON_PUBLICDEC void random_init(Random seed);
-COMMON_PUBLICDEC Random random_get_current_seed(void);
-COMMON_PUBLICDEC Random random_lc(void);
-COMMON_PUBLICDEC Random random_xor_shift(void);
-COMMON_PUBLICDEC Random random_number(void);
-COMMON_PUBLICDEC f32 random_f32(void);
+RANDOM_PUBLICDEC void random_init(Random seed);
+RANDOM_PUBLICDEC Random random_get_current_seed(void);
+RANDOM_PUBLICDEC Random random_lc(void);
+RANDOM_PUBLICDEC Random random_xor_shift(void);
+RANDOM_PUBLICDEC Random random_number(void);
+RANDOM_PUBLICDEC f32 random_f32(void);
 
 #endif // _RANDOM_H
 
@@ -20,17 +30,17 @@ COMMON_PUBLICDEC f32 random_f32(void);
 
 static Random current_seed = 2147483647;
 
-COMMON_PUBLICDEF
+RANDOM_PUBLICDEF
 void random_init(Random seed) {
   current_seed = seed;
 }
 
-COMMON_PUBLICDEF
+RANDOM_PUBLICDEF
 Random random_get_current_seed(void) {
   return current_seed;
 }
 
-COMMON_PUBLICDEF
+RANDOM_PUBLICDEF
 Random random_lc(void) {
   const Random a = 16807;
   const Random multiplier = 2147483647;
@@ -38,7 +48,7 @@ Random random_lc(void) {
   return (current_seed = (current_seed * a + increment) % multiplier);
 }
 
-COMMON_PUBLICDEF
+RANDOM_PUBLICDEF
 Random random_xor_shift(void) {
   current_seed ^= current_seed << 13;
   current_seed ^= current_seed >> 17;
@@ -46,12 +56,12 @@ Random random_xor_shift(void) {
   return current_seed;
 }
 
-COMMON_PUBLICDEF
+RANDOM_PUBLICDEF
 Random random_number(void) {
   return random_lc();
 }
 
-COMMON_PUBLICDEF
+RANDOM_PUBLICDEF
 f32 random_f32(void) {
   return (i32)random_number() / (f32)INT32_MAX;
 }
